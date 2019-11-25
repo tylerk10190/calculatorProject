2be09206849ff35bb0f85af50a6aad71f69f65e9
tylerk10190 <t.koepk10190@gmail.com>
2019-11-25 16:46:44 -0600

    justified text on the screen to the right. Added the logic for division for two single digit numbers. Still working on logic for bigger numbers.

--- myCalculator.js
+++ myCalculator.js
@@ -4,7 +4,20 @@ function buttonClick(entry){
   if(entry == null){
     document.getElementById('myScreen').innerHTML = "";
   }
+  else if(entry == "="){
+    if(document.getElementById('myScreen').innerHTML.includes("/")){
+    var myScreenString =  document.getElementById('myScreen').innerHTML.toString();
+    var num1 = myScreenString.slice(0,1);
+    var num2 = myScreenString.slice(2,3);
+    document.getElementById('myScreen').innerHTML = num1 / num2;
+    }
+  }
   else{
     document.getElementById('myScreen').innerHTML += entry;
   }
+}
+
+function addTwoNumbers(num1, num2){
+  result = num1 + num2;
+  console.log(result)
 }
